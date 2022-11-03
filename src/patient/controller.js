ab6b512ac18f91f4f5e44645450ada3fb221e65f
Carolina Vitória <carolinav.chavesilva@gmail.com>
2022-11-03 15:30:53 -0300

    Rotas de update e delete ok.

--- src/patient/controller.js
+++ src/patient/controller.js
@@ -31,13 +31,82 @@ router.post("/patientregistry", (req, res) => {
     });
 });
 
-router.get("/patientlist", (res) => {
+router.get("/patientlist", (req, res) => {
   patient
     .findAll()
     .then((patients) => {
       res.status(200).send(patients);
     })
-    .catch((error) => res.status(404).json({ error: error }));
+    .catch((error) => {
+      res.status(404).json({ error: "Ainda não há pacientes para mostrar." });
+    });
+});
+
+router.get("/patientlist:id", (req, res) => {
+  let { id } = req.params;
+
+  patient
+    .findAll({
+      where: {
+        id,
+      },
+    })
+    .then((patient) => {
+      res.status(200).send(patient);
+    })
+    .catch((error) => {
+      res.status(404).json({ error: "Paciente náo cadastrado." });
+    });
+});
+
+router.put("/putpatient/:id", (req, res) => {
+  let {
+    id,
+    name,
+    telephone,
+    smartphone,
+    email,
+    responsibleName,
+    responsiblePhone,
+  } = req.body;
+
+  patient
+    .update(
+      {
+        name,
+        telephone,
+        smartphone,
+        email,
+        responsibleName,
+        responsiblePhone,
+      },
+      {
+        where: id,
+      }
+    )
+    .then(() =>
+      res.status(200).json({ message: "Dados de paciente editados." })
+    )
+    .catch((error) => {
+      res.status(500).json({ error: "Não foi possível editar dados." });
+    });
+});
+
+router.delete("/deletepatient/:id", (req, res) => {
+  let { id } = req.params;
+
+  patient
+    .destroy({
+      where: {
+        id,
+      },
+    })
+    .then(() =>
+      res.status(200).json({ message: "Paciente deletado com sucesso." })
+    )
+    .catch((error) => {
+      res.status(500).json({ error: "Não foi possível deletar." });
+    });
 });
 
 module.exports = router;
